e9533a08422e6ad7632fa8b095b31a638573696b
phbr13 <phborges2006@gmail.com>
2023-12-30 17:28:25 -0300

    admin page criada e senha criada

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// tabbar entradas e saidas para trocar de página
 function entrou(rota) {
     document.querySelector(`body > footer > a:nth-child(${rota}) > svg > path`).style.fill = "#f56e00ff"
 }
 function saiu(rota) {
     document.querySelector(`body > footer > a:nth-child(${rota}) > svg > path`).style.fill = "#f5f4f5"
+}
+
+//verificação de senha para entrar na página de adm
+function verificarSenha() {
+    const senha = document.querySelector("#senha").value
+    if(senha === "salamaleiko") {
+        window.location.href = "admin_dashboard.html"
+    } else {
+        const senhaerrada = document.querySelector("#aviso")
+        senhaerrada.style.animation = "senhaerrada normal 5s"
+        setTimeout(() => {
+            document.querySelector("#aviso").style.animation = ""
+        }, 5050);
+    }
 }
