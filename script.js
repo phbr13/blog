000ad022ec1147aeb3b491dc1a481a635b3fbb6c
phbr13 <phborges2006@gmail.com>
2024-01-06 15:35:56 -0300

    mudanças muito loucaaaaaaaaaaaaaaaaaaaaaaaaas

--- script.js
+++ script.js
@@ -65,88 +65,12 @@ function verificarSenha(AdmJaConectado) {
     });
 }
 
-//parte do dashboard (pesquisa do conteudo)
-function pesquisarFilme() {
-    const filme = document.querySelector("#searchbar").value
-    const resultadoPesquisa = document.querySelector("#resultado-pesquisa")
-    resultadoPesquisa.innerHTML=''
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjZlMTM5MjFiODIwYWFhZmE2YTY5MjYwNDlmMmUwNyIsInN1YiI6IjY1OTViMmJiMzI2ZWMxMDY3MTA2YzE4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7JFdsfoWvql9rEP_XyrKv2SB8HpXbvJHg4o9bDhYc14'
-        }
-    };
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${filme}&include_adult=false&language=pt-BR&page=1`, options)
-    .then(response => response.json())
-    .then(response => {
-        response.results.forEach(element => {
-            let generos 
-            if (element.genre_ids.length === 0) {
-                generos = "sem gêneros"
-            } else {
-                generos = identificarGen(element.genre_ids)
-            }
-            let imgFilme
-            if (element.poster_path === null) {
-                imgFilme = 'imgs/none.jpg'
-            } else {
-                imgFilme = `https://image.tmdb.org/t/p/w500${element.poster_path}`
-            }
-            let posterFilme
-            if (element.backdrop_path === null) {
-                posterFilme = 'imgs/none2.png'
-            } else {
-                posterFilme = `https://image.tmdb.org/t/p/w500${element.backdrop_path}`
-            }
-            let data
-            if (element.release_date === "") {
-                data = 'sem data'
-            } else {
-                data = `${element.release_date}`
-            }
-            let overview
-            if (element.overview === "") {
-                overview = 'Sem resumo'
-            } else {
-                overview = `${element.overview}`
-            }
-            resultadoPesquisa.innerHTML += `
-            <div class="resultado">
-                <img src="${imgFilme}" alt="">
-                <h5>${element.title} <span style="font-size: 13px; color: #797979;font-style: italic;">(${data})</span></h5>
-                <input class="btn" type="button" value="Detalhes" onclick="aparecerDetals(${element.id})">
-            </div>
-            <div class="card-movie-detals" id="${element.id}">
-            <div class="fundo-detals" onclick="desaparecerDetals(${element.id})"></div>
-            <div class="card-detals movie${element.id}">
-                <img style="border: 0;" src="${posterFilme}" alt="">
-                <div style="padding-right: 13px;">
-                    <h5>${element.title}</h5>
-                    <h5 style="margin-top: -5px;"><span style="font-size: 13px; color: #797979;font-style: italic;">${element.original_title} (${data})</span></h5>
-                    <h5 style="margin-top: -5px;"><span style="font-size: 13px; color: #797979;font-style: italic;">Gênero(s): ${generos}</span></h5>
-                    <h5 style="font-size: 18px;">Overview</h5> 
-                    <p style="margin-bottom: 0; font-size: 16px;">${overview}</p>
-                    <h5 style="font-size: 18px">Avaliação</h5>
-                    <h5 style="margin-top: -5px;margin-bottom: 13px;"><span style="font-size: 13px; color: #797979;font-style: italic;">${element.vote_average.toFixed(1)}/10 de acordo com ${element.vote_count} usuário(s) do <a href="https://www.themoviedb.org/" target="_blank" style="color: #797979;">TMDB</a></span></h5>
-                </div>
-            </div>
-            </div>
-            `
-        });
-    })
-    .catch(err => console.error(err));
-}
-function pesquisarLivro() {
-    console.log(document.querySelector("#searchbar").value)
-}
-
-//funcoes para aparecer os detalhes dos filmes
+//funcoes para aparecer e desaparecer os detalhes dos filmes/series
 function aparecerDetals(id) {
-    const detalsFilme = document.getElementById(`${id}`)
-    const cardDetal = document.querySelector(`.movie${id}`)
-    detalsFilme.style.display = 'flex'
-    gsap.to(detalsFilme, {
+    const detals = document.getElementById(`${id}`)
+    const cardDetal = document.querySelector(`.obra${id}`)
+    detals.style.display = 'flex'
+    gsap.to(detals, {
         opacity: 1,
         delay: 0.3,
         duration: 0.1,
@@ -158,11 +82,10 @@ function aparecerDetals(id) {
         duration: 0.1,
     })
 }
-
 function desaparecerDetals(id) {
-    const detalsFilme = document.getElementById(`${id}`)
-    const cardDetal = document.querySelector(`.movie${id}`)
-    gsap.to(detalsFilme, {
+    const detals = document.getElementById(`${id}`)
+    const cardDetal = document.querySelector(`.obra${id}`)
+    gsap.to(detals, {
         opacity: 0,
         delay: 0.3,
         duration: 0.1,
@@ -174,98 +97,126 @@ function desaparecerDetals(id) {
         duration: 0.1,
     })
     setTimeout(() => {
-        detalsFilme.style.display = 'none'
+        detals.style.display = 'none'
     }, 350);
 }
 
-//identificador de generos de cinema
-const generos = [
-    {
-      id: 28,
-      name: "Ação"
-    },
-    {
-      id: 12,
-      name: "Aventura"
-    },
-    {
-      id: 16,
-      name: "Animação"
-    },
-    {
-      id: 35,
-      name: "Comédia"
-    },
-    {
-      id: 80,
-      name: "Crime"
-    },
-    {
-      id: 99,
-      name: "Documentário"
-    },
-    {
-      id: 18,
-      name: "Drama"
-    },
-    {
-      id: 10751,
-      name: "Família"
-    },
-    {
-      id: 14,
-      name: "Fantasia"
-    },
-    {
-      id: 36,
-      name: "História"
-    },
-    {
-      id: 27,
-      name: "Terror"
-    },
-    {
-      id: 10402,
-      name: "Música"
-    },
-    {
-      id: 9648,
-      name: "Mistério"
-    },
-    {
-      id: 10749,
-      name: "Romance"
-    },
-    {
-      id: 878,
-      name: "Ficção científica"
-    },
-    {
-      id: 10770,
-      name: "Cinema TV"
-    },
-    {
-      id: 53,
-      name: "Thriller"
-    },
-    {
-      id: 10752,
-      name: "Guerra"
-    },
-    {
-      id: 37,
-      name: "Faroeste"
+//identificador de generos de filmes e series
+let generosFilmes
+fetch('data/generosFilmes.json')
+.then(response => response.json())
+.then(data => {
+    generosFilmes = data})
+.catch(error => console.error('Erro ao carregar o arquivo JSON:', error));
+let generosSeries
+fetch('data/generosSeries.json')
+.then(response => response.json())
+.then(data => {
+    generosSeries = data})
+.catch(error => console.error('Erro ao carregar o arquivo JSON:', error));
+function identificarGen(genres, especificar) {
+    let generos = ''
+    let armazemGeneros
+    if (especificar === 'movie') {
+        armazemGeneros = generosFilmes
+    } else {
+        armazemGeneros = generosSeries
     }
-]
-function identificarGen(geners) {
-    let generosFIlmes = ''
-    for (let index1 = 0; index1 < geners.length; index1++) {
-        const element = geners[index1];
-        for (let index = 0; index < generos.length; index++) {
-            if (element == generos[index].id) {
-                generosFIlmes += `${generos[index].name}, `
+    for (let index1 = 0; index1 < genres.length; index1++) {
+        const element = genres[index1];
+        for (let index = 0; index < armazemGeneros.length; index++) {
+            if (element == armazemGeneros[index].id) {
+                generos += `${armazemGeneros[index].name}, `
             }
         }
     }
-    return generosFIlmes.slice(0, -2)
+    return generos.slice(0, -2)
+}
+
+//display do resultado da pesquisa generico para filmes e series
+function displayContent(especificar) {
+    const search = document.querySelector("#searchbar").value
+    const resultadoPesquisa = document.querySelector("#resultado-pesquisa")
+    resultadoPesquisa.innerHTML=''
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjZlMTM5MjFiODIwYWFhZmE2YTY5MjYwNDlmMmUwNyIsInN1YiI6IjY1OTViMmJiMzI2ZWMxMDY3MTA2YzE4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7JFdsfoWvql9rEP_XyrKv2SB8HpXbvJHg4o9bDhYc14'
+        }
+    };
+    fetch(`https://api.themoviedb.org/3/search/${especificar}?query=${search}&include_adult=false&language=pt-BR&page=1`, options)
+    .then(response => response.json())
+    .then(response => {
+        response.results.forEach(element => {
+            let data = 'sem data'
+            let nome = {
+                original: '',
+                brasileiro: ''
+            }
+            if (especificar === "tv") {
+                if (element.first_air_date !== "") {
+                    data = `${element.first_air_date}`
+                }
+                nome.original = element.original_name
+                nome.brasileiro = element.name
+            } else {
+                if (element.release_date !== "") {
+                    data = `${element.release_date}`
+                }
+                nome.original = element.original_title
+                nome.brasileiro = element.title
+            }
+            let generos = "sem gêneros"
+            if (element.genre_ids.length !== 0) {
+                generos = identificarGen(element.genre_ids, especificar)
+            }
+            let img = 'imgs/none.jpg'
+            if (element.poster_path !== null) {
+                img = `https://image.tmdb.org/t/p/w500${element.poster_path}`
+            }
+            let poster = 'imgs/none2.png'
+            if (element.backdrop_path !== null) {
+                poster = `https://image.tmdb.org/t/p/w500${element.backdrop_path}`
+            }
+            let overview = 'Sem sinopse'
+            if (element.overview !== "") {
+                overview = `${element.overview}`
+            }
+            resultadoPesquisa.innerHTML += `
+            <div class="resultado">
+                <img src="${img}" alt="">
+                <h5>${nome.brasileiro} <span style="font-size: 13px; color: #797979;font-style: italic;">(${data})</span></h5>
+                <input class="btn" type="button" value="Detalhes" onclick="aparecerDetals(${element.id})">
+            </div>
+            <div class="card-obra-detals" id="${element.id}">
+            <div class="fundo-detals" onclick="desaparecerDetals(${element.id})"></div>
+            <div class="card-detals obra${element.id}">
+                <img style="border: 0;" src="${poster}" alt="">
+                <div style="padding-right: 13px;">
+                    <h5>${nome.brasileiro}</h5>
+                    <h5 style="margin-top: -5px;"><span style="font-size: 13px; color: #797979;font-style: italic;">${nome.original} (${data})</span></h5>
+                    <h5 style="margin-top: -5px;"><span style="font-size: 13px; color: #797979;font-style: italic;">Gênero(s): ${generos}</span></h5>
+                    <h5 style="font-size: 18px;">Sinopse</h5> 
+                    <p style="margin-bottom: 0; font-size: 16px;">${overview}</p>
+                    <h5 style="font-size: 18px">Avaliação</h5>
+                    <h5 style="margin-top: -5px;margin-bottom: 13px;"><span style="font-size: 13px; color: #797979;font-style: italic;">${element.vote_average.toFixed(1)}/10 de acordo com ${element.vote_count} usuário(s) do <a href="https://www.themoviedb.org/" target="_blank" style="color: #797979;">TMDB</a></span></h5>
+                </div>
+            </div>
+            </div>
+            `
+        });
+    })
+    .catch(err => console.error(err));
+}
+
+//parte do dashboard (pesquisa do conteudo)
+function pesquisarFilme() {
+    displayContent('movie')
+}
+function pesquisarSerie() {
+    displayContent('tv')
+}
+function pesquisarLivro() {
+    console.log(document.querySelector("#searchbar").value)
 }
